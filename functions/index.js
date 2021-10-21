65636049511f76daa1af0db45664a9775567a0e2
JS.Park <kocat53@naver.com>
2021-10-21 16:42:58 +0900

    netlify 파일 오타 수정

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 exports.handler = async function (event, context) {
     return {
-        stateCode : 200,
+        statusCode : 200,
         // body moon자만 입력이 가ㅇㅐㅓ 제이ㄴ 써야 함
         body: JSON.stringify({
             name: 'park',
